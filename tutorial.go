42e2ddb29838d1f2ad93e415816c83b91a29e985
Ji-Ha <xodhkd36@gmail.com>
2020-11-09 21:56:45 +0900

    practice echo

--- tutorial.go
+++ tutorial.go
@@ -1,7 +1,10 @@
+//Form multipart/form-data까지 했음.
 package main
 
 import (
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 )
@@ -13,5 +16,45 @@ func main() {
 		return c.String(http.StatusOK, "Hello World!")
 	})
 
+	e.GET("/users/:id", getUser)
+	e.GET("/show", show)
+	e.POST("/save", save)
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func show(c echo.Context) error {
+	//팀과 맴버를 쿼리 string에서 가저옵니다.
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team: "+team+", member: "+member)
+}
+
+func save(c echo.Context) error {
+	name := c.FormValue("name")
+	avatar, err := c.FormFile("avatar")
+	if err != nil {
+		return err
+	}
+
+	src, err := avatar.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Create(avatar.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+	return c.String(http.StatusOK, "<b>Thank you! "+name+"</b>")
 }
